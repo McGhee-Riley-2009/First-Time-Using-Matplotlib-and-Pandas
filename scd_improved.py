e145b787af76ce3b242164ff61ca67448940c587
Riley McGhee <mchughr009@gmail.com>
2025-11-18 22:22:21 -0500

    Update scd_improved.py

--- scd_improved.py
+++ scd_improved.py
@@ -1,17 +1,18 @@
-import improved_module_scd as ims
+import custom_module as cm
 
-ims.set_window_size(20, 10)
-df = ims.load_data("SteamData.csv")
+cm.set_window_size(20, 10)
+df = cm.load_data("SteamData.csv")
 
-ims.horizontal_guidelines(df, "negative_reviews", 10000)
+cm.horizontal_guidelines(df, "negative_reviews", 10000)
 
-ims.plot_certain_columns(df, "game", "positive_reviews")
-ims.show_plot()
+cm.plot_certain_columns(df, "game", "positive_reviews")
+cm.show_plot()
 
-ims.plot_certain_columns(df, "game", "negative_reviews")
+cm.plot_certain_columns(df, "game", "negative_reviews")
 
-ims.set_labels("Games sorted by reviews")
-ims.xticks_config(15)
-ims.show_plot()
+cm.set_labels("Games sorted by reviews")
+cm.xticks_config(15)
+cm.show_plot()
+
 
 
